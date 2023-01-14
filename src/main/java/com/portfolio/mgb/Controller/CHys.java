4e9a45cf357999f486e5b75b2a4ad8667cff3570
Mariano Ressio <marianoressio@gmail.com>
2023-01-14 03:56:43 -0300

    firebase url added

--- src/main/java/com/portfolio/mgb/Controller/CHys.java
+++ src/main/java/com/portfolio/mgb/Controller/CHys.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://mgbfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-final-12c87.web.app/","http://localhost:4200"})
 @RequestMapping("/skill")
 public class CHys {
 
--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://mgbfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontend-final-12c87.web.app/","http://localhost:4200"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
